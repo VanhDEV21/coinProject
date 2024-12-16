c3e0407077311b3e40f866c13de47250a946d7fa
Nguyen Viet Anh <vanhspmax@gmail.com>
2024-12-16 15:06:54 +0700

    update mongo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const coinRoutes = require('./routes/coinRoute')
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-const MONGO_URI = 'mongodb://127.0.0.1:27017/myCoindatabase';
+const MONGO_URI = 'mongodb+srv://vanhspmax:kwtipzJDhhHjWbFb@productioncluster.23sze.mongodb.net/myCoindatabase?retryWrites=true&w=majority';
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true , serverSelectionTimeoutMS: 30000 })
 .then(()=>console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error: '))
