fae72067ba7c2c81d99cd4dc02898c83b7369834
Nguyen Viet Anh <vanhspmax@gmail.com>
2024-12-16 15:33:46 +0700

    add

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const MONGO_URI = 'mongodb+srv://vanhspmax:kwtipzJDhhHjWbFb@productioncluster.23sze.mongodb.net/myCoindatabase?retryWrites=true&w=majority';
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true , connectTimeoutMS: 30000, socketTimeoutMS: 45000,serverSelectionTimeoutMS: 5000 })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true , connectTimeoutMS: 60000, socketTimeoutMS: 60000,serverSelectionTimeoutMS: 60000 })
 .then(()=>console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error: '))
 
